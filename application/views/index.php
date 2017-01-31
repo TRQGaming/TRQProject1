42b33ad16cb65b1850c0e1a59c2ac1d9c38d697a
mynameisryan <rymy1515@gmail.com>
2017-01-31 15:34:22 -0800

    views

--- application/views/index.php
+++ application/views/index.php
@@ -2,8 +2,22 @@
 <html>
 <head>
 	<title>All Users</title>
-</head>
+	<link rel="stylesheet" href="/assets/stylesheets/style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"></head>
 <body>
-	<h1>Users index page coming from the Users controller Index Method
+	<div class="container" id="loginbackground">
+
+		<div class="jumbotron col-sm-10 col-lg-5 col-md-4" id="logindetails" >
+			<h2 id="login">Game  <span class="glyphicon glyphicon-cloud" ></span></h2>
+			<form class="" id="login" action="" method="post">
+				Email: <input class="pushsomeright col-sm-10 col-lg-6 col-md-4" type="email" name="email">
+				Password: <input class="pushsomeright col-sm-10 col-lg-6 col-md-4" type="text" name="password">
+				<input type="submit" name="submit" value="Login">
+			</form>
+		</div>
+	</div>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+	<script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
 </body>
 </html>
