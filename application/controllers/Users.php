fbf62ece055d14a2db5001bee6120045a2218102
TedC <warofted@g.ucla.edu>
2017-01-31 15:14:46 -0800

    added user_register()

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -3,23 +3,43 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Users extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
 	{
+		$this->output->enable_profiler();
+
 		$this->load->view('index');
 	}
+
+	public function user_register()
+	{
+		$first_name = $this->input->post('first_name');
+		$last_name = $this->input->post('last_name');
+		$user_name = $this->input->post('user_name');
+		$email = $this->input->post('email');
+		$password = md5($this->input->post('password'));
+
+		$this->load->library("form_validation");
+		$this->form_validation->set_rules("first_name", "last_name", "user_name", "trim|required");
+		$this->form_validation->set_rules("email", "trim|valid_email|required");
+		$this->form_validation->set_rules("password", "password_conf", "trim|min_length[8]|required|matches[confirm_password]|md5");
+		if($this->form_validation->run() === FALSE)
+		{
+			$this->session->error("login_errors", validation_errors());
+			redirect(base_url());
+		}
+		else
+		{
+		$this->load->model("user");
+		$register = $this->input->post();
+		$user_register = $this->users->user_input($register);
+		$this->load->view("index");
+		}
+	}
+
+
+
+
+
+
+
 }
