5a917e1870c045cecb119d7acaf32d9d67dd00e3
mynameisryan <rymy1515@gmail.com>
2017-01-31 16:41:06 -0800

    login view done

--- application/views/index.php
+++ application/views/index.php
@@ -5,22 +5,22 @@
 	<link rel="stylesheet" href="/assets/stylesheets/style.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"></head>
 <body>
-	<div class="container" id="loginbackground">
-		<div class="col-sm-0 col-lg-1 col-md-2">
+	<div class="container back" id="loginbackground">
+		<div class="col-sm-4 col-lg-5 col-md-4">
 		</div>
-
-		<div class="jumbotron col-sm-10 col-lg-6 col-md-9" id="logindetails" >
+		<div class="jumbotron col-sm-4 col-lg-2 col-md-4" id="logindetails" >
 			<h2 id="login">Game  <span class="glyphicon glyphicon-cloud" ></span></h2>
+			<div class="middle">
 			<form class="col-sm-12 col-lg-12 col-md-12" id="login" action="" method="post">
-				<span class="col-sm-12 col-lg-5 col-md-5">Email: <input type="email" name="email"></span>
-				<span class="col-sm-12 col-lg-5 col-md-5">Password: <input type="text" name="password"></span>
-				<span class="col-sm-12 col-lg-1 col-md-0"><input type="submit" name="submit" value="Login"></span>
+				<span class="col-sm-12 col-lg-12 col-md-12"><input type="email" name="email" placeholder="Email" ></span>
+				<span class="col-sm-12 col-lg-12 col-md-12"><input type="text" name="password" placeholder="password" ></span>
+				<span class="col-sm-12 col-lg-12 col-md-12"><input class="btn btn-warning" id="down" type="submit" name="submit" value="Login"></span>
 			</form>
+			</div>
 		</div>
-		<div class="col-sm-0 col-lg-1 col-md-2">
+		<div class="col-sm-4 col-lg-5 col-md-4">
 		</div>
 	</div>
-
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 	<script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
 </body>
