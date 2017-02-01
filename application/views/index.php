257c4677b4ffd91f9cf3c6488fccbf039ec1a77e
mynameisryan <rymy1515@gmail.com>
2017-01-31 16:03:26 -0800

    Views

--- application/views/index.php
+++ application/views/index.php
@@ -6,14 +6,18 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"></head>
 <body>
 	<div class="container" id="loginbackground">
+		<div class="col-sm-0 col-lg-1 col-md-2">
+		</div>
 
-		<div class="jumbotron col-sm-10 col-lg-5 col-md-4" id="logindetails" >
+		<div class="jumbotron col-sm-10 col-lg-6 col-md-9" id="logindetails" >
 			<h2 id="login">Game  <span class="glyphicon glyphicon-cloud" ></span></h2>
-			<form class="" id="login" action="" method="post">
-				Email: <input class="pushsomeright col-sm-10 col-lg-6 col-md-4" type="email" name="email">
-				Password: <input class="pushsomeright col-sm-10 col-lg-6 col-md-4" type="text" name="password">
-				<input type="submit" name="submit" value="Login">
+			<form class="col-sm-12 col-lg-12 col-md-12" id="login" action="" method="post">
+				<span class="col-sm-12 col-lg-5 col-md-5">Email: <input type="email" name="email"></span>
+				<span class="col-sm-12 col-lg-5 col-md-5">Password: <input type="text" name="password"></span>
+				<span class="col-sm-12 col-lg-1 col-md-0"><input type="submit" name="submit" value="Login"></span>
 			</form>
+		</div>
+		<div class="col-sm-0 col-lg-1 col-md-2">
 		</div>
 	</div>
 
